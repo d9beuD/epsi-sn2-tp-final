96db58ab1a1df42f317ed73b35c5117321f7c211
d9beuD <d9beuD@users.noreply.github.com>
2022-10-26 23:18:35 +0200

    Possibilité d'ajouter/mettre à jour l'artiste

--- src/Controller/ArtistController.php
+++ src/Controller/ArtistController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Artist;
+use App\Repository\ArtistRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -16,6 +18,33 @@ class ArtistController extends AbstractController
         $artist = $doctrine->getRepository(Artist::class)->find(1);
 
         return $this->render('artist/index.html.twig', [
+            'artist' => $artist,
+        ]);
+    }
+
+    #[Route('/artist/new', methods: ['GET', 'POST'])]
+    public function setArtist(Request $request, ArtistRepository $artistRepository, ManagerRegistry $doctrine): Response
+    {
+        $entityManager = $doctrine->getManager();
+        $artist = $artistRepository->find(1);
+
+
+        if ($request->getMethod() === 'POST') {
+            if ($artist === null) {
+                $artist = new Artist();
+            }
+
+            $artist->setName($request->request->get('name'))
+                ->setIllustrationURL($request->request->get('illustrationURL'))
+                ->setDescription($request->request->get('description'));
+
+            $entityManager->persist($artist); // Ne fonctionne que si l'artiste n'existe pas déjà
+            $entityManager->flush();
+
+            return $this->redirectToRoute('home');
+        }
+
+        return $this->render('artist/new.html.twig', [
             'artist' => $artist,
         ]);
     }
