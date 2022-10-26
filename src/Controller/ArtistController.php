529a07b3c729e97556dcb627be2d6309971b5061
d9beuD <d9beuD@users.noreply.github.com>
2022-10-26 23:18:01 +0200

    L'affichage de l'artiste devient la page d'accueil

--- src/Controller/ArtistController.php
+++ src/Controller/ArtistController.php
@@ -2,17 +2,21 @@
 
 namespace App\Controller;
 
+use App\Entity\Artist;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ArtistController extends AbstractController
 {
-    #[Route('/', name: 'app_artist')]
-    public function index(): Response
+    #[Route('/', name: 'home')]
+    public function index(ManagerRegistry $doctrine): Response
     {
+        $artist = $doctrine->getRepository(Artist::class)->find(1);
+
         return $this->render('artist/index.html.twig', [
-            'controller_name' => 'ArtistController',
+            'artist' => $artist,
         ]);
     }
 }
